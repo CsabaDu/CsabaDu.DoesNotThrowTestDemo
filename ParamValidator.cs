ec42c1b1c39b9ad8e1bec30e50dae76d6ae28b00
CsabaDu <csabadudas@t-email.hu>
2024-06-19 10:11:11 +0200

    Add project files.

---
+++ ParamValidator.cs
@@ -0,0 +1,39 @@
+﻿namespace CsabaDu.DoesNotThrowTestDemo;
+
+public class ParamValidator
+{
+    public void Validate([DoesNotReturnIf(false)] bool isValid)
+    {
+        //ArgumentOutOfRangeException.ThrowIfNotEqual(isValid, true, nameof(isValid));
+
+        if (isValid) return;
+
+        throw new ArgumentOutOfRangeException(nameof(isValid));
+    }
+
+    private void DoSomething(object? value)
+    {
+        throw new Exception();
+    }
+
+    private object? _newProperty;
+
+    public object? NewProperty
+    {
+        get => _newProperty;
+        set
+        {
+            try
+            {
+                DoSomething(value);
+
+                _newProperty = value;
+            }
+            catch (Exception)
+            {
+                _newProperty = null;
+            }
+        }
+    }
+
+}
